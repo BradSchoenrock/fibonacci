dc1cfc2dfb672d70931fd0a6e730f1f32f4e7685
bschoenrock <bschoenrock@localhost.localdomain>
2019-01-29 13:21:45 -0700

    starting

---
+++ fib.py
@@ -0,0 +1,17 @@
+
+import os
+import sys
+import datetime
+
+
+
+def main():
+    print ("Hello World: Current time:",datetime.datetime.now())
+
+    # do stuff here 
+
+    print ("Goodnight World: Current time:",datetime.datetime.now())
+
+
+if __name__ == '__main__':
+    main()
